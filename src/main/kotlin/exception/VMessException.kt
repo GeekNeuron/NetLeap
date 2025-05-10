af3e0f0f6a560a593181adbdf68471a9fcb29af4
Geek Neuron <geekneuron@gmail.com>
2025-05-10 22:18:10 +0330

    VMessException.kt

---
+++ src/main/kotlin/exception/VMessException.kt
@@ -0,0 +1,7 @@
+package GeekNeuron.client.exception
+
+/**
+ * A custom exception for VMess protocol errors.
+ * @param message The error message.
+ */
+class VMessException(message: String) : Exception(message)
