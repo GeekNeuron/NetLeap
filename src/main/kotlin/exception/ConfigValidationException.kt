0d5b3512629679b19607bdee06f40e1fcc9e34dd
Geek Neuron <geekneuron@gmail.com>
2025-05-10 22:17:39 +0330

    ConfigValidationException.kt

---
+++ src/main/kotlin/exception/ConfigValidationException.kt
@@ -0,0 +1,7 @@
+package GeekNeuron.client.exception
+
+/**
+ * A custom exception for configuration validation errors.
+ * @param message The error message.
+ */
+class ConfigValidationException(message: String) : Exception(message)
